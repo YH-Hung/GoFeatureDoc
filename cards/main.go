e4691dbf8db3c69852c05f12e7f9c7771228a59a
yinghanhung <hle.infdy@gmail.com>
2025-08-15 22:03:24 +0800

    add deck proj

---
+++ cards/main.go
@@ -0,0 +1,17 @@
+package main
+
+var card string = "latter assign"
+
+func main() {
+	// cards := newDeck()
+
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// fmt.Println("-----")
+	// remainingCards.print()
+
+	// cards.saveToFile("my_cards")
+
+	cards := newDeckFromFile("my_cards")
+	cards.print()
+}
